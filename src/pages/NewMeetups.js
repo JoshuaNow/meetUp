ddfe0a26bf07c2a409e630bfc9867bdb779f42a0
Joshua Lopez <jrenelopez92@gmail.com>
2022-02-17 14:44:07 -0600

    JL: navi programmatically

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,11 +1,26 @@
-import React from "react";
+import { useHistory } from "react-router-dom";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetups() {
+  const history = useHistory();
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-getting-started-b7dce-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    ).then(() => {
+      history.replace("/");
+    });
+  }
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
